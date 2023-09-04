d24926ab2ce67fca4f1b0b1e389006015d2baf9a
Kilvny <108346469+Kilvny@users.noreply.github.com>
2023-09-05 01:09:26 +0300

    tasks component created

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctor Appointment',
+    day: 'May 5th at 7:00pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Have breakfast',
+    day: 'May 6th at 8:00am',
+    reminder: false,
+  },
+  {
+    id: 3,
+    text: 'Put food in the freezer',
+    day: 'May 5th at 1:30pm',
+    reminder: false,
+  },
+];
