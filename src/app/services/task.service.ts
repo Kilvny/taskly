6229fc69d9ac1e2561e79f749eb4cbb1247862e6
Kilvny <108346469+Kilvny@users.noreply.github.com>
2023-09-12 17:30:12 +0300

    update: added headers to httpClient requests

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
   private apiUrl = "http://localhost:5000/tasks";
+  headers: HttpHeaders = new HttpHeaders({
+    "content-type": "application/json; charset=utf-8"
+  })
 
   constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = this.http.get<Task[]>(this.apiUrl)
+    const tasks = this.http.get<Task[]>(this.apiUrl, {
+      headers: this.headers
+    })
     return tasks;
   }
 
   addTask(task: Task): Observable<Task> {
-    const postedTask = this.http.post<Task>(this.apiUrl,task)
+    const postedTask = this.http.post<Task>(this.apiUrl,task, {
+      headers: this.headers
+    })
     return postedTask
   }
 
